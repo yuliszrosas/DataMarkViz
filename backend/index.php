6e412d39d41def2267b23384edcfc4d78d96cdb4
Yuliset Sanzón Rosas <yulisetsanzonrosas@gmail.com>
2026-04-01 02:13:38 -0600

    Instalación de SlimPHP y agregar index.php

---
+++ backend/index.php
@@ -0,0 +1,15 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+$app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write('Hello World!');
+    return $response;
+});
+
+$app->run();
