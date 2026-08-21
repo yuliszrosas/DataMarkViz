bdf3a28ba8f9311e0e0e70102b2d17ce036aa429
Yuliset Sanzón Rosas <yulisetsanzonrosas@gmail.com>
2026-08-21 02:14:20 -0600

    feat(backend): usar variable ALLOWED_ORIGIN para CORS en vez de localhost fijo

--- backend/index.php
+++ backend/index.php
@@ -21,7 +21,7 @@ $app->options('/{routes:.+}', function ($request, $response, $args) {
 $app->add(function ($request, $handler) {
     $response = $handler->handle($request);
     return $response
-        ->withHeader('Access-Control-Allow-Origin', 'http://localhost:5173')
+        ->withHeader('Access-Control-Allow-Origin', $_ENV['ALLOWED_ORIGIN'] ?? 'http://localhost:5173')
         ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
         ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
 });
